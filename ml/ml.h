ad09a155cbe1e18f501e2851097e79e1b4503e9f
eltonlima01 <eltoncvlima@gmail.com>
2026-04-10 16:43:06 -0300

    Minor changes

--- ml/ml.h
+++ ml/ml.h
@@ -5,7 +5,7 @@ typedef struct LinearModel LinearModel;
 LinearModel *newLM(const float param_0, const float param_1);
 void deleteLM(LinearModel **linearModel);
 
-float predict(const LinearModel *linearModel, float x);
+float predict(const LinearModel *linearModel, const float x);
 
 float getParam_0(const LinearModel *linearModel);
 float getParam_1(const LinearModel *linearModel);
