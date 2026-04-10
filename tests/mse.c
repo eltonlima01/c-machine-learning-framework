1f5b4223137e34d0ce264721193e5b908e8f7177
eltonlima01 <eltoncvlima@gmail.com>
2026-04-10 19:26:11 -0300

    MSE calculation test

---
+++ tests/mse.c
@@ -0,0 +1,22 @@
+// ================================================================ //
+// MSE calculation test //
+// ================================================================ //
+
+#include "../ml/ml.h"
+
+#include <stdio.h>
+
+int main(void)
+{
+    LinearModel *linearModel = newLM(1.0f, 3.0f);
+    Dataset *dataset = newDataset("datasets/AI_Student_Life_Pakistan_2026.csv", "Age", "Daily_Usage_Hours", 20);
+
+    printf("Linear model params: %.2f, %.2f\n", getParam_0(linearModel), getParam_1(linearModel));
+
+    printf("Mean squared error: %.2f\n", MSE(dataset, linearModel));
+
+    deleteLM(&linearModel);
+    deleteDataset(&dataset);
+
+    return 0;
+}
