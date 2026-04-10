1ec63dcbee7f98328917e6871f16d8c27fd16bfb
eltonlima01 <eltoncvlima@gmail.com>
2026-04-10 00:20:21 -0300

    Linear regression model & prediction tests

---
+++ tests/linear.c
@@ -0,0 +1,20 @@
+// ================================================================ //
+// Linear regression model & prediction tests //
+// ================================================================ //
+
+#include "../ml/ml.h"
+#include <stdio.h>
+
+int main(void)
+{
+    LinearModel *linearModel = newLM(3.75f, 6.78e-5f);
+
+    puts("Cyprus -> PIB US$ 32.655:");
+
+    printf("Life satisfaction = %.2f + (6,78x10⁻⁵)xPIB\n= %.2f + (6,78x10⁻⁵)x32655\n= %.2f\n", getParam_0(linearModel),
+           getParam_0(linearModel), predict(linearModel, 32655.0f));
+
+    deleteLM(&linearModel);
+
+    return 0;
+}
