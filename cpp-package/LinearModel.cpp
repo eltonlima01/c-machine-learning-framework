4fc1be4644d98e22dfb8c9f7d4b021b2ecce6189
eltonlima01 <eltoncvlima@gmail.com>
2026-04-15 19:47:30 -0300

    Minor changes

--- cpp-package/LinearModel.cpp
+++ cpp-package/LinearModel.cpp
@@ -1,11 +1,11 @@
-#include <math_kernels.h>
 #include <ml.hpp>
+#include <math_kernels.h>
 
 namespace ML
 {
-void LinearModel::train(const float *paramX, const float *paramY, const int samples, const float trainingRate,
-                        const int epochs)
+void LinearModel::train(const Dataset &dataset, const float trainingRate, const int epochs)
 {
-    ::train(&param0, &param1, paramX, paramY, samples, trainingRate, epochs);
+    ::train(&this->param0, &this->param1, dataset.getParamXData(), dataset.getParamYData(), dataset.getDatasetSize(),
+            trainingRate, epochs);
 }
 } // namespace ML
